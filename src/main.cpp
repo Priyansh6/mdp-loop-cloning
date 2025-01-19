17cb8c4422a99729aca9b1ca674bf4fec2ab3b94
Priyansh Mahajan <priyanshmahajan@gmail.com>
2025-01-19 14:54:42 +0000

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#if 0
+#include <array>
+
+std::array<int, 1000> l;
+std::array<int, 1000> k;
+
+void test_aliases(int* a, int* b, std::size_t n)
+{
+  for (std::size_t i = 0; i < n; i++) {
+    a[i] = b[i];
+    b[i] += 1;
+  }
+}
+
+void test_aliases_cloned(int* a, int* b, std::size_t n)
+{
+  if (a == b) {
+    for (std::size_t i = 0; i < n; i++) {
+      a[i] = b[i];
+      b[i] += 1;
+    }
+  } else {
+    for (std::size_t i = 0; i < n; i++) {
+      a[i] = b[i];
+      b[i] += 1;
+    }
+  }
+}
+#endif
+
+int main()
+{
+  // test_aliases(&l.front(), &k.front(), 1000);
+  // test_aliases(&l.front(), &l.front(), 1000);
+
+  // test_aliases_cloned(&l.front(), &k.front(), 1000);
+  // test_aliases_cloned(&l.front(), &l.front(), 1000);
+  int a = 1;
+  return 0;
+}
